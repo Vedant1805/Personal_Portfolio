86ba87c4269701e154f58cbec844c592b738c907
Vedant Dabhade <vedantdabhade174@gmail.com>
2024-03-06 01:06:04 +0530

    Update Project.jsx

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -6,7 +6,7 @@ import { FaArrowCircleRight } from "react-icons/fa";
 
 const Project = () => {
     return (
-        <div className="project section" id='project'>
+        <div className="project section" id='projects'>
             <h2 className="section__title">Projects</h2>
             <span className='section__subtitle'>Latest Projects</span>
 
@@ -17,11 +17,11 @@ const Project = () => {
                         <img src={projectimg1} alt="" />
                         <div className="project__data">
                             <div className="project__info">
-                                <p>CryptoCurrency stalker</p>
-                                <p>html css javascript</p>
+                                <p>Movie Booking App</p>
+                                <p>Javascript ReactJs Redux MERN</p>
                             </div>
 
-                            <a href="https://github.com/Vedant1805/IMDB_CLONE" rel="noreferrer" target='_blank' >
+                            <a href="https://github.com/Vedant1805/movie-booking-project.git" rel="noreferrer" target='_blank' >
                                 <FaArrowCircleRight className='right-icon' >
                                 </FaArrowCircleRight>
                             </a>
@@ -33,7 +33,7 @@ const Project = () => {
                         <div className="project__data">
                             <div className="project__info">
                                 <p>IMDB Clone</p>
-                                <p>html css javascript</p>
+                                <p>Javascript ReactJs, MaterialUI</p>
                             </div>
 
                             <a href="https://imdb-clone-t2sd.onrender.com/" rel="noreferrer" target='_blank' >
